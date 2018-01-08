a0a843260d1478c9722beef6de686e9d7557f088
James Dunne <james.jdunne@gmail.com>
2018-01-08 17:15:50 -0600

    Rename gl package.

--- gles2_test.go
+++ gles2_test.go
@@ -3,14 +3,14 @@ package gles2_test
 import "testing"
 
 import "github.com/JamesDunne/rpi-egl/bcm"
-import "github.com/JamesDunne/rpi-egl/gles2"
+import gl "github.com/JamesDunne/rpi-egl/gles2"
 
 func TestOpenGLInit(t *testing.T) {
 	display := bcm.OpenDisplay()
 	if display == nil {
 		t.Fatal("display = nil")
 	}
-	err := gles2.Init()
+	err := gl.Init()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -23,11 +23,11 @@ func TestClear(t *testing.T) {
 		t.Fatal("display = nil")
 	}
 	defer display.Close()
-	err := gles2.Init()
+	err := gl.Init()
 	if err != nil {
 		t.Fatal(err)
 	}
-	gles2.ClearColor(0.10, 0.33, 0.33, 1.0)
-	gles2.Clear(gles2.COLOR_BUFFER_BIT)
+	gl.ClearColor(0.10, 0.33, 0.33, 1.0)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
 	display.SwapBuffers()
 }
